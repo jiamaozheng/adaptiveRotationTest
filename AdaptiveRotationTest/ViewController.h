ba7f20f4a722fd334534fae4c93fe7111a990d65
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-09 10:39:53 -0500

    sdfasdf

--- AdaptiveRotationTest/ViewController.h
+++ AdaptiveRotationTest/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIView *cicle;
+@property (weak, nonatomic) IBOutlet UILabel *line;
 
 @end
 
--- AdaptiveRotationTest/ViewController.m
+++ AdaptiveRotationTest/ViewController.m
@@ -16,7 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.cicle = [[UIView alloc] init];
+    self.cicle.backgroundColor = [UIColor blueColor];
+    
+    CAShapeLayer *shape = [CAShapeLayer layer];
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:self.cicle.center radius:(self.cicle.bounds.size.width / 2) startAngle:0 endAngle:(2 * M_PI) clockwise:YES];
+    shape.path = path.CGPath;
+    self.cicle.layer.mask = shape;
+    
+//    [self.icon setFont:[UIFont fontWithName:@"person" size:65]];
+//    [self.icon setText:@"3"];
+//    self.icon.textColor = [UIColor personBlueColor];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextAddEllipseInRect(ctx, rect);
+    CGContextSetFillColor(ctx, CGColorGetComponents([[UIColor blueColor] CGColor]));
+    CGContextFillPath(ctx);
 }
 
 - (void)didReceiveMemoryWarning {
